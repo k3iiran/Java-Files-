95a02aa57d3ac338e1456cc64da4e011252d1d8c
keiran <lexi sucks>
2019-10-02 11:28:21 +0100

    java updates

--- Accounts.java
+++ Accounts.java
@@ -1,11 +1,8 @@
 class Accounts{
-	public void taxcalculation(int salary){
-		float tax;
-		tax=salary*21/100;
-		System.out.println("your tax"+tax);
-		}
-	public float tax(int salary){
-		float t=salary*21/100;
-		return t;
+	public static void main(String[] args){
+	Accounts2 ac=new Accounts2();
+	ac.tax();
+	ac.tax(2000);
+	ac.tax(3000,15);
 	}
 }
--- Accounts2.java
+++ Accounts2.java
@@ -1,12 +1,13 @@
 class Accounts2{
-	public static void main(String[]args){
-	Accounts ac=new Accounts();
-	String name;
-	int salary;
-	float tax,net;
-	name="james";
-	salary=(1000);
-	net=salary-ac.tax(salary);
-	System.out.println(net);
+	public void tax(){
+		System.out.println("tax rates:"+21);
+	}
+	public void tax(int salary){
+		float tax=(float)salary*21/100;
+		System.out.println("your tax is:"+tax);
+	}
+	public void tax(int sal,int rate){
+		float tax=(float)sal*rate/100;
+		System.out.println("your tax is:"+tax);
 	}
 }
